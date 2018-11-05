db114a28f63692e535a4f600a556093135f85bb3
daiquocnguyen <dai.nguyen@monash.edu>
2018-11-05 13:03:23 +1100

    Update model.py

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class ConvKB(object):
 
     def __init__(self, sequence_length, num_classes, embedding_size, filter_sizes, num_filters, vocab_size,
                  pre_trained=[], l2_reg_lambda=0.001, is_trainable=True, useConstantInit=False):
-        # Placeholders for input, output and dropout
+        # Placeholders for input, output and dropout, not using a batch size in placeholder
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
