253a10d67d20a792a33771ad3412dfce83c28405
Dai Quoc Nguyen <dai.nguyen@monash.edu>
2020-06-12 23:03:32 +1000

    Update ConvKB pytorch

---
+++ ConvKB_pytorch/ConvKB.py
@@ -0,0 +1,89 @@
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+from Model import Model
+from numpy.random import RandomState
+
+torch.manual_seed(123)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(123)
+
+class ConvKB(Model):
+
+    def __init__(self, config):
+        super(ConvKB, self).__init__(config)
+
+        self.ent_embeddings = nn.Embedding(self.config.entTotal, self.config.hidden_size)  # vectorized quaternion
+        self.rel_embeddings = nn.Embedding(self.config.relTotal, self.config.hidden_size)
+
+        self.conv1_bn = nn.BatchNorm2d(1)
+        self.conv_layer = nn.Conv2d(1, self.config.out_channels, (self.config.kernel_size, 3))  # kernel size x 3
+        self.conv2_bn = nn.BatchNorm2d(self.config.out_channels)
+        self.dropout = nn.Dropout(self.config.convkb_drop_prob)
+        self.non_linearity = nn.ReLU()
+        self.fc_layer = nn.Linear((self.config.hidden_size - self.config.kernel_size + 1) * self.config.out_channels, 1, bias=False)
+
+        self.criterion = nn.Softplus()
+        self.init_parameters()
+
+    def init_parameters(self):
+        if self.config.use_init_embeddings == False:
+            nn.init.xavier_uniform_(self.ent_embeddings.weight.data)
+            nn.init.xavier_uniform_(self.rel_embeddings.weight.data)
+
+        else:
+            self.ent_embeddings.weight.data = self.config.init_ent_embs
+            self.rel_embeddings.weight.data = self.config.init_rel_embs
+
+        nn.init.xavier_uniform_(self.fc_layer.weight.data)
+        nn.init.xavier_uniform_(self.conv_layer.weight.data)
+
+    def _calc(self, h, r, t):
+        h = h.unsqueeze(1) # bs x 1 x dim
+        r = r.unsqueeze(1)
+        t = t.unsqueeze(1)
+
+        conv_input = torch.cat([h, r, t], 1)  # bs x 3 x dim
+        conv_input = conv_input.transpose(1, 2)
+        # To make tensor of size 4, where second dim is for input channels
+        conv_input = conv_input.unsqueeze(1)
+        conv_input = self.conv1_bn(conv_input)
+        out_conv = self.conv_layer(conv_input)
+        out_conv = self.conv2_bn(out_conv)
+        out_conv = self.non_linearity(out_conv)
+        out_conv = out_conv.view(-1, (self.config.hidden_size - self.config.kernel_size + 1) * self.config.out_channels)
+        input_fc = self.dropout(out_conv)
+        score = self.fc_layer(input_fc).view(-1)
+
+        return -score
+
+    def loss(self, score, regul):
+        return torch.mean(self.criterion(score * self.batch_y)) + self.config.lmbda * regul
+
+    def forward(self):
+        h = self.ent_embeddings(self.batch_h)
+        r = self.rel_embeddings(self.batch_r)
+        t = self.ent_embeddings(self.batch_t)
+        score = self._calc(h, r, t)
+
+        # regularization
+        l2_reg = torch.mean(h ** 2) + torch.mean(t ** 2) + torch.mean(r ** 2)
+        for W in self.conv_layer.parameters():
+            l2_reg = l2_reg + W.norm(2)
+        for W in self.fc_layer.parameters():
+            l2_reg = l2_reg + W.norm(2)
+
+        return self.loss(score, l2_reg)
+
+    def predict(self):
+
+        h = self.ent_embeddings(self.batch_h)
+        r = self.rel_embeddings(self.batch_r)
+        t = self.ent_embeddings(self.batch_t)
+        score = self._calc(h, r, t)
+
+        return score.cpu().data.numpy()
---
+++ ConvKB_pytorch/train_ConvKB.py
@@ -0,0 +1,153 @@
+from Config import Config
+from ConvKB import ConvKB
+import json
+import os
+import numpy as np
+
+from argparse import ArgumentParser
+parser = ArgumentParser("QTransE")
+parser.add_argument("--dataset", default="WN18RR", help="Name of the dataset.")
+parser.add_argument("--learning_rate", default=0.05, type=float, help="Learning rate")
+parser.add_argument("--nbatches", default=100, type=int, help="Number of batches")
+parser.add_argument("--num_epochs", default=300, type=int, help="Number of training epochs")
+parser.add_argument("--model_name", default='WN18RR', help="")
+parser.add_argument('--neg_num', default=10, type=int, help='')
+parser.add_argument('--hidden_size', type=int, default=50, help='')
+parser.add_argument('--num_of_filters', type=int, default=64, help='')
+parser.add_argument('--dropout', type=float, default=0.5, help='')
+parser.add_argument('--save_steps', type=int, default=1000, help='')
+parser.add_argument('--valid_steps', type=int, default=50, help='')
+parser.add_argument("--lmbda", default=0.1, type=float, help="")
+parser.add_argument("--lmbda2", default=0.01, type=float, help="")
+parser.add_argument("--mode", choices=["train", "predict"], default="train", type=str)
+parser.add_argument("--checkpoint_path", default=None, type=str)
+parser.add_argument("--test_file", default="", type=str)
+parser.add_argument("--optim", default='adagrad', help="")
+parser.add_argument('--use_init', default=1, type=int, help='')
+parser.add_argument('--kernel_size', default=1, type=int, help='')
+args = parser.parse_args()
+
+if args.model_name is None or len(args.model_name.strip()) == 0:
+    args.model_name = "{}_lda-{}_nneg-{}_hs-{}_lr-{}_nepochs-{}".format(args.dataset,
+                                                                        args.lmbda,
+                                                                        args.neg_num,
+                                                                        args.hidden_size,
+                                                                        args.learning_rate,
+                                                                        args.num_epochs)
+print(args)
+
+out_dir = os.path.abspath(os.path.join("../runs_pytorch_ConvKB/"))
+print("Writing to {}\n".format(out_dir))
+# Checkpoint directory
+checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+result_dir = os.path.abspath(os.path.join(checkpoint_dir, args.model_name))
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+if not os.path.exists(result_dir):
+    os.makedirs(result_dir)
+
+con = Config()
+in_path = "./benchmarks/" + args.dataset + "/"
+con.set_in_path(in_path)
+test_file_path = ""
+if args.test_file != "":
+    test_file_path = in_path + args.test_file
+con.set_test_file_path(test_file_path)
+con.set_work_threads(8)
+con.set_train_times(args.num_epochs)
+con.set_nbatches(args.nbatches)
+con.set_alpha(args.learning_rate)
+con.set_bern(1)
+con.set_dimension(args.hidden_size)
+con.set_lmbda(args.lmbda)
+con.set_lmbda_two(0.01)
+con.set_margin(1.0)
+con.set_ent_neg_rate(args.neg_num)
+con.set_opt_method(args.optim)
+con.set_save_steps(args.save_steps)
+con.set_valid_steps(args.valid_steps)
+con.set_early_stopping_patience(10)
+con.set_checkpoint_dir(checkpoint_dir)
+con.set_result_dir(result_dir)
+# set link prediction
+con.set_test_link(True)
+# set triple classification
+con.set_test_triple(False)
+con.init()
+
+def get_term_id(filename):
+    entity2id = {}
+    id2entity = {}
+    with open(filename) as f:
+        for line in f:
+            if len(line.strip().split()) > 1:
+                tmp = line.strip().split()
+                entity2id[tmp[0]] = int(tmp[1])
+                id2entity[int(tmp[1])] = tmp[0]
+    return entity2id, id2entity
+
+def get_init_embeddings(relinit, entinit):
+    lstent = []
+    lstrel = []
+    with open(relinit) as f:
+        for line in f:
+            tmp = [float(val) for val in line.strip().split()]
+            lstrel.append(tmp)
+    with open(entinit) as f:
+        for line in f:
+            tmp = [float(val) for val in line.strip().split()]
+            lstent.append(tmp)
+    return np.array(lstent, dtype=np.float32), np.array(lstrel, dtype=np.float32)
+
+
+if args.mode == "train":
+
+    if args.use_init:
+        hidden_size = "100"
+        con.set_dimension(100)
+        if args.dataset == "WN18RR":
+            hidden_size = "50"
+            con.set_dimension(50)
+
+        init_entity_embs, init_relation_embs = get_init_embeddings(
+            "./benchmarks/" + args.dataset + "/relation2vec"+hidden_size+".init",
+            "./benchmarks/" + args.dataset + "/entity2vec"+hidden_size+".init")
+
+        e2id, id2e = get_term_id(filename="./benchmarks/" + args.dataset + "/entity2id.txt")
+        e2id50, id2e50 = get_term_id(filename="./benchmarks/" + args.dataset + "/entity2id_"+hidden_size+"init.txt")
+        assert len(e2id) == len(e2id50)
+
+        entity_embs = np.empty([len(e2id), con.hidden_size]).astype(np.float32)
+        for i in range(len(e2id)):
+            _word = id2e[i]
+            id = e2id50[_word]
+            entity_embs[i] = init_entity_embs[id]
+
+        r2id, id2r = get_term_id(filename="./benchmarks/" + args.dataset + "/relation2id.txt")
+        r2id50, id2r50 = get_term_id(filename="./benchmarks/" + args.dataset + "/relation2id_"+hidden_size+"init.txt")
+        assert len(r2id) == len(r2id50)
+
+        rel_embs = np.empty([len(r2id), con.hidden_size]).astype(np.float32)
+        for i in range(len(r2id)):
+            _rel = id2r[i]
+            id = r2id50[_rel]
+            rel_embs[i] = init_relation_embs[id]
+
+        con.set_init_embeddings(entity_embs, rel_embs)
+
+    con.set_config_CNN(num_of_filters=args.num_of_filters, drop_prob=args.dropout, kernel_size=args.kernel_size)
+    con.set_train_model(ConvKB)
+    con.training_model()
+
+else:
+    if args.use_init:
+        hidden_size = "100"
+        con.set_dimension(100)
+        if args.dataset == "WN18RR":
+            hidden_size = "50"
+            con.set_dimension(50)
+
+    con.set_config_CNN(num_of_filters=args.num_of_filters, drop_prob=args.dropout, kernel_size=args.kernel_size)
+    con.set_test_model(ConvKB, args.checkpoint_path)
+    con.test()
+
